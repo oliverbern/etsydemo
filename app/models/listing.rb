2894fd32aba3b49ac6a07e1c4c1ec0018d3528b1
Oliver Jones <oapjones@gmail.com>
2014-09-04 21:57:58 +0200

    add banner to homepage

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,4 @@
 class Listing < ActiveRecord::Base
-  self.table_name = "listings"
   if Rails.env.development?
     has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
     validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png)

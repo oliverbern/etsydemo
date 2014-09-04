f72032dfcc23a96ea65527b2ee28f041abdbb81b
Oliver Jones <oapjones@gmail.com>
2014-09-04 20:37:38 +0200

    fix error in models/listing.rb

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,16 +3,13 @@ class Listing < ActiveRecord::Base
   if Rails.env.development?
     has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
     validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png)
-    else
+  else
     has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
     :storage => :dropbox,
     :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
     :path => ":style/:id_:filename"
-  validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png)
-  class User < ActiveRecord::Base
-
-  end  
-end
+    validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png)
+  end
 
   validates :name, :description, :price, presence: true
   validates :price, numericality: { greater_than: 0}

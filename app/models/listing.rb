6f290f050174e7962cdf475a8aaaca8f179e4133
Oliver Jones <oapjones@gmail.com>
2014-09-06 15:31:21 +0200

    Buy process

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -16,4 +16,6 @@ class Listing < ActiveRecord::Base
 
   belongs_to :user
 
+  has_many :orders
+
 end
---
+++ db/migrate/20140906130254_add_field_to_orders.rb
@@ -0,0 +1,6 @@
+class AddFieldToOrders < ActiveRecord::Migration
+  def change
+    add_column :orders, :buyer_id, :integer
+    add_column :orders, :seller_id, :integer
+  end
+end
